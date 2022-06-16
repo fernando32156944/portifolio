4c9d11b89d1843211b06d9c1ce25c9ef65f918c8
Fernando <102802997+fernando32156944@users.noreply.github.com>
2022-06-16 10:29:08 -0300

    Rename paralel programing/exercicio 2.c to paralel programing/remembering C/exercicio 2.c

--- paralel programing/remembering C/exercicio 2.c
+++ paralel programing/remembering C/exercicio 2.c
@@ -4,9 +4,9 @@
 
 * TIA: 32156944
 
-* Escreva um programa que leia 3 notas de um aluno e a média das notas dos exercícios realizados por ele. 
-  Calcular a média de aproveitamento, usando a fórmula: MA = (N1 + N2*2 + N3*3 + ME)/7. 
-  A partir da média, informar o conceito de acordo com a tabela:
+* Escreva um programa que leia 3 notas de um aluno e a mÃ©dia das notas dos exercÃ­cios realizados por ele. 
+  Calcular a mÃ©dia de aproveitamento, usando a fÃ³rmula: MA = (N1 + N2*2 + N3*3 + ME)/7. 
+  A partir da mÃ©dia, informar o conceito de acordo com a tabela:
   maior ou igual a 9	A
   maior ou igual a 7.5 e menor que 9	B
   maior ou igual a 6 e menor que 7.5	C
